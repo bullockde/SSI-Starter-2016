f55a58bee0e063e3b0d0886ce39113d46a07fceb
Darius Bullock <bullockde@vcu.edu>
2018-12-07 17:39:46 -0500

    3rd Initial Commit - Full Site

--- content-none.php
+++ content-none.php
@@ -1,31 +1,33 @@
 <?php
 /**
- * The template for displaying a "No posts found" message
+ * The template part for displaying a message that posts cannot be found
  *
  * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
  */
 ?>
 
-<header class="page-header">
-	<h1 class="page-title"><?php _e( 'Nothing Found', 'twentyfourteen' ); ?></h1>
-</header>
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php _e( 'Nothing Found', 'twentysixteen' ); ?></h1>
+	</header><!-- .page-header -->
 
-<div class="page-content">
-	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+	<div class="page-content">
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentyfourteen' ), admin_url( 'post-new.php' ) ); ?></p>
+			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentysixteen' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
-	<?php elseif ( is_search() ) : ?>
+		<?php elseif ( is_search() ) : ?>
 
-	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyfourteen' ); ?></p>
-	<?php get_search_form(); ?>
+			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentysixteen' ); ?></p>
+			<?php get_search_form(); ?>
 
-	<?php else : ?>
+		<?php else : ?>
 
-	<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentyfourteen' ); ?></p>
-	<?php get_search_form(); ?>
+			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentysixteen' ); ?></p>
+			<?php get_search_form(); ?>
 
-	<?php endif; ?>
-</div><!-- .page-content -->
+		<?php endif; ?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
